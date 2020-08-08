81fb3891d413d2628ada061c3484b0a4fd76c2d7
Pavel Valena <pvalena@redhat.com>
2020-08-08 18:15:56 +0200

    Break test suite - TEST.

--- spec/abrt_spec.rb
+++ spec/abrt_spec.rb
@@ -6,7 +6,7 @@ describe 'ABRT' do
     output_message_pattern = /\A#{abrt_rb}:\d+:in `<main>': can't modify frozen Array(: \[1, 2, 3\])? \((FrozenError|RuntimeError)\)\n\Z/
 
     it 'disabled' do
-      expect { system "ruby --disable-gems #{abrt_rb}" }
+      expect { system "ruby - -disable-gems #{abrt_rb}" }
         .to output(/\A\Z/).to_stdout_from_any_process
         .and output(output_message_pattern).to_stderr_from_any_process
     end
